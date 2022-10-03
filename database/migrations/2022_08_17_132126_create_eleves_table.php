d1884b0b4e9608dbd2eb7c2db64cd458ed85a40c
Mounzz <senmounia@hotmail.com>
2022-10-03 11:43:12 +0200

    first commit

---
+++ database/migrations/2022_08_17_132126_create_eleves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eleves', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 50);
+            $table->string('prénom', 40);
+            $table->string('email')->nullable();
+            $table->string('adresse', 200);
+            $table->date('date de naissance');
+            $table->ipAddress('adress ip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eleves');
+    }
+};
